bfe6c5dad5313836ecdf9e72657278d045448cb8
dan-galano <dan.galano.dev@gmail.com>
2025-07-23 18:49:14 +0800

    feat: implement token creation and cookie handling in signup and login functions

--- server/controllers/auth.controller.mjs
+++ server/controllers/auth.controller.mjs
@@ -1,4 +1,5 @@
 import User from "../models/user.model.mjs";
+import jwt from "jsonwebtoken";
 
 export const fetchUsers = async (req, res) => {
   try {
@@ -9,6 +10,20 @@ export const fetchUsers = async (req, res) => {
   }
 };
 
+const maxAge = 3 * 24 * 60 * 60;
+
+export const createRefreshToken = (payload) => {
+  return jwt.sign(payload, process.env.JWT_REFRESH_TOKEN_SECRET, {
+    expiresIn: "30d",
+  });
+};
+
+export const createAccessToken = (payload) => {
+  return jwt.sign(payload, process.env.JWT_ACCESS_TOKEN_SECRET, {
+    expiresIn: "1h",
+  });
+};
+
 export const signup = async (req, res) => {
   const { email, password } = req.body;
 
@@ -17,7 +32,33 @@ export const signup = async (req, res) => {
     if (!user) {
       return res.status(500).json({ message: "Failed to create user." });
     }
-    res.status(200).json(user);
+
+    const refreshToken = createRefreshToken({
+      id: user._id,
+      email: user.email,
+    });
+
+    const accessToken = createAccessToken({
+      id: user._id,
+      email: user.email,
+    });
+
+    res.cookie("refreshToken", refreshToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: true,
+    });
+
+    res.cookie("accessToken", accessToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "strict",
+      maxAge: 60 * 60 * 1000,
+    });
+
+    console.log({ user });
+
+    res.status(201).json({ accessToken });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
@@ -26,7 +67,17 @@ export const signup = async (req, res) => {
 export const login = async (req, res) => {
   const { email, password } = req.body;
 
-  res.status(200).json(req.body);
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: "User not found." });
+    }
+
+    return res.status(200).json({ message: "user exists.", user });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Server error." });
+  }
 };
 
 export const logout = async (req, res) => {
