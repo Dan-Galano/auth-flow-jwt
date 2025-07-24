e22b2c428b083d6dded90186811085c206af44d6
dan-galano <dan.galano.dev@gmail.com>
2025-07-24 17:03:04 +0800

    feat: enhance Home component layout with improved styling and structure

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,14 +1,22 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div>
-      <p className='text-h5'>Home</p>
-      <Link to={'/login'}><span className='relative hover-underline'>Login</span></Link><br />
-      <Link to={'/signup'}><span className='relative hover-underline'>Signup</span></Link>
+    <div className="grid place-items-center h-dvh text-center">
+      <section className="flex flex-col gap-2">
+        <p className="text-h5">Home</p>
+        <div className="flex gap-4">
+          <Link to={"/login"}>
+            <span className="relative hover-underline">Login</span>
+          </Link>
+          <Link to={"/signup"}>
+            <span className="relative hover-underline">Signup</span>
+          </Link>
+        </div>
+      </section>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
