70c8740c8a2cc8d17fa951ef9e99406188b54666
dan-galano <dan.galano.dev@gmail.com>
2025-07-24 17:03:27 +0800

    feat: add cookie utility functions for user authentication and token management

---
+++ client/src/utils/cookie.js
@@ -0,0 +1,30 @@
+import api from "./axios";
+
+export const getUserFromCookie = async () => {
+  try {
+    const response = await api.get("/api/user-info");
+    return response;
+  } catch (error) {
+    if (error.response && error.response.status === 401) {
+      console.warn("User not logged in.");
+      return null;
+    }
+    console.error("Failed to fetch user", error);
+    return null;
+  }
+};
+
+export const refreshToken = async () => {
+  try {
+    const response = await api.get("/api/refresh-token");
+    return response.data.accessToken;
+  } catch (error) {
+    window.location.href = "/login";
+    console.log("Failed to refresh token: ", error);
+    if (error.response && error.response.status === 401) {
+      console.warn("Refresh token expired. Logging out...");
+      return null;
+    }
+    return null;
+  }
+};
