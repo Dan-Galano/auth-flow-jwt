431621e567056f4979d827a16276e4ac2044c60e
dan-galano <dan.galano.dev@gmail.com>
2025-07-24 17:03:34 +0800

    feat: add token verification middleware for JWT authentication

---
+++ server/middlewares/verify.token.mjs
@@ -0,0 +1,34 @@
+import jwt from "jsonwebtoken";
+
+const verifyToken = (req, res, next) => {
+  const accessToken = req.cookies.accessToken;
+
+  if (!accessToken) {
+    const authHeader = req.headers.authorization;
+    if(authHeader && authHeader.startsWith('Bearer ')){
+      accessToken = authHeader.split(' ')[1];
+    }
+  }
+
+  if (!accessToken) {
+    return res.status(401).json({ message: "Access denied. No token provided." });
+  }
+
+  jwt.verify(
+    accessToken,
+    process.env.JWT_ACCESS_TOKEN_SECRET,
+    (error, decodedToken) => {
+      if (error) {
+        console.log("Token error:", error);
+        return res
+          .status(403)
+          .json({ message: "Access denied. Invalid token." });
+      }
+
+      req.user = decodedToken; // attach user info to request
+      next();
+    }
+  );
+};
+
+export default verifyToken;
