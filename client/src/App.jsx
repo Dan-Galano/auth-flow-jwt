d97c2f7640b8eae5a2ed6e3c10d79b29e993f59a
dan-galano <dan.galano.dev@gmail.com>
2025-07-24 17:02:54 +0800

    feat: restructure routing to include protected routes for dashboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,14 +3,19 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+import Dashboard from "./pages/Dashboard";
+import ProtectedRoutes from "./routes/ProtectedRoutes";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/signup" element={<Signup/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route element={<ProtectedRoutes />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
