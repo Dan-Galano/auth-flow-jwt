0d181cc4ac2eeb9e5e9b6b6ced751c262dbe00dc
dan-galano <dan.galano.dev@gmail.com>
2025-07-24 17:03:41 +0800

    feat: update user routes to fetch user info and refresh token with verification

--- server/routes/auth.route.mjs
+++ server/routes/auth.route.mjs
@@ -1,17 +1,21 @@
 import express from "express";
 import {
-  fetchUsers,
+  fetchUser,
+  refreshToken,
   login,
   logout,
   signup,
 } from "../controllers/auth.controller.mjs";
+import verifyToken from "../middlewares/verify.token.mjs";
 import { recaptcha } from "../middlewares/recaptcha.mjs";
 
 const router = express.Router();
 
-router.get("/users", fetchUsers);
+router.get("/user-info", verifyToken, fetchUser);
+router.get("/refresh-token", refreshToken);
 router.post("/signup", recaptcha, signup);
 router.post("/login", recaptcha, login);
 router.post("/logout", logout);
+
 
 export default router;
