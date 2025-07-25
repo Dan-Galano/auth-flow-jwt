aba7620ded1182338bb5a7e8a4d2ce68d000a5d1
dan-galano <dan.galano.dev@gmail.com>
2025-07-26 01:53:28 +0800

    FEAT: Add loader for logout

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import api from "../utils/axios";
 import { toast, Toaster } from "sonner";
 import { useStore } from "../stores/auth.store";
+import Loader from "../components/Loader";
 
 const Dashboard = () => {
   const navigate = useNavigate();
   const user = useStore((state) => state.user);
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleLogout = async () => {
     try {
+      setIsLoading(true);
       const response = await api.post("/api/logout");
       if (response.status === 200) {
         navigate("/login");
@@ -19,12 +22,15 @@ const Dashboard = () => {
       }
     } catch (error) {
       console.log(error);
+    } finally {
+      setIsLoading(false);
     }
   };
 
   return (
     <div className="grid place-items-center h-dvh text-center">
       <section className="flex flex-col gap-4">
+        {isLoading ? <Loader /> : <></>}
         <Toaster />
         <div className="flex flex-col gap-1">
           <p className="text-h5">DGLN Dashboard</p>
